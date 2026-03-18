bb332437e393561fc3780f095b3a16b2841a5b33
Vaidulla <vajdullaak2@gmail.com>
2026-03-18 11:26:09 +0300

    Отправка отчета

---
+++ StudentManager.Tests/StudentManagerTests.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+
+[TestFixture]
+public class StudentManagerTests
+{
+    private StudentManager studentManager;
+
+    [SetUp]
+    public void Setup()
+    {
+        studentManager = new StudentManager();
+    }
+
+    [Test]
+    public void AddStudent_ShouldAddStudent()
+    {
+        var student = new Student("Alice", 1);
+        studentManager.AddStudent(student);
+
+        var result = studentManager.GetStudent(1);
+        Assert.That(result.Name, Is.EqualTo("Alice")); // Исправлено
+    }
+
+    [Test]
+    public void RemoveStudent_ShouldRemoveStudent()
+    {
+        var student = new Student("Bob", 2);
+        studentManager.AddStudent(student);
+        studentManager.RemoveStudent(2);
+
+        var result = studentManager.GetStudent(2);
+        Assert.That(result, Is.Null); // Исправлено
+    }
+
+    [Test]
+    public void GetAllStudents_ShouldReturnAllStudents()
+    {
+        studentManager.AddStudent(new Student("Alice", 1));
+        studentManager.AddStudent(new Student("Bob", 2));
+
+        var students = studentManager.GetAllStudents();
+        Assert.That(students.Count, Is.EqualTo(2)); // Исправлено
+    }
+}
---
+++ StudentManager/StudentManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+public class Student
+{
+    public string Name { get; set; }
+    public int Id { get; set; }
+
+    public Student(string name, int id)
+    {
+        Name = name;
+        Id = id;
+    }
+}
+
+public class StudentManager
+{
+    private List<Student> students = new List<Student>();
+
+    public void AddStudent(Student student)
+    {
+        students.Add(student);
+    }
+
+    public void RemoveStudent(int id)
+    {
+        var student = students.Find(s => s.Id == id);
+        if (student != null)
+        {
+            students.Remove(student);
+        }
+    }
+
+    public Student GetStudent(int id)
+    {
+        return students.Find(s => s.Id == id);
+    }
+
+    public List<Student> GetAllStudents()
+    {
+        return new List<Student>(students);
+    }
+}
